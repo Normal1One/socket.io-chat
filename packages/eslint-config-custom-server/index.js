7d0fc7fad47f98e266ec366a0c805f0234e4e152
Nikita Lavrov <nlavrov2007@gmail.com>
2023-09-15 12:24:56 +0200

    fix: pnpm changed to yarn

--- packages/eslint-config-custom-server/index.js
+++ packages/eslint-config-custom-server/index.js
@@ -15,6 +15,9 @@ module.exports = {
 			}
 		}
 	],
+	rules: {
+		'@typescript-eslint/no-var-requires': 'off'
+	},
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
